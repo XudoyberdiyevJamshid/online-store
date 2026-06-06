1e34270974cecb3831871a497883cd164a2e90d2
jamshid <jamshiddev1@gmail.com>
2026-06-06 19:07:42 +0500

    feat: enhance product detail and listing pages with improved UI and functionality
    - Added a loading state to the product detail page.
    - Improved the layout and styling of the navigation bar.
    - Enhanced product card design with better visuals and buttons.
    - Implemented a search input with improved styling in the product listing page.
    - Added a server script to package.json for easier local development.
    - Updated dependencies for Tailwind CSS and json-server.

--- src/app/features/mahsulot-detail/mahsulot-detail.ts
+++ src/app/features/mahsulot-detail/mahsulot-detail.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Mahsulot } from '../../core/models/mahsulot.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MahsulotService } from '../../core/services/mahsulot';
+import { SavatService } from '../../core/services/savat';
 
 @Component({
   selector: 'app-mahsulot-detail',
@@ -6,4 +10,38 @@ import { Component } from '@angular/core';
   templateUrl: './mahsulot-detail.html',
   styleUrl: './mahsulot-detail.css',
 })
-export class MahsulotDetail {}
+export class MahsulotDetail implements OnInit {
+  mahsulot = signal<Mahsulot | null>(null);
+  yuklanyapti = signal(true);
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private mahsulotService: MahsulotService,
+    private savatService: SavatService,
+  ) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.mahsulotService.mahsulotniOl(id).subscribe({
+      next: (data) => {
+        this.mahsulot.set(data);
+        this.yuklanyapti.set(false);
+      },
+      error: () => {
+        this.router.navigate(['/']);
+      },
+    });
+  }
+
+  savatgaQosh() {
+    if (this.mahsulot()) {
+      this.savatService.qoshish(this.mahsulot()!);
+      this.router.navigate(['/savat']);
+    }
+  }
+
+  ortgaQayt() {
+    this.router.navigate(['/']);
+  }
+}
